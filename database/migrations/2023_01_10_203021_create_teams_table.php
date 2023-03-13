a9b7576fdc0cfdf966ebab1e591265785367f0ee
hala <halaibrahim867@gmail.com>
2023-03-13 02:54:57 +0200

    add header and footer components

---
+++ database/migrations/2023_01_10_203021_create_teams_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->string('job_description');
+            $table->string('facebook_account')->nullable();
+            $table->string('instagram_account')->nullable();
+            $table->string('twitter_account')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+};
